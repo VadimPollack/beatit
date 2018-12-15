db23fd8cd3756bcbc70a3af1714e3535382e6e07
Dan Man <daniel.gyarmaty@gmx.de>
2018-12-15 08:04:54 +0100

    Implemented List in main content view

--- mobile/src/main/java/de/uni_freiburg/iems/beatit/EventDiary.java
+++ mobile/src/main/java/de/uni_freiburg/iems/beatit/EventDiary.java
@@ -4,12 +4,28 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.Arrays;
+
 public class EventDiary extends AppCompatActivity {
+
+    String [] smokeList = {
+            "Adidas - Kurs: 73,45 €",
+            "Allianz - Kurs: 145,12 €",
+            "BASF - Kurs: 84,27 €",
+            "Bayer - Kurs: 128,60 €",
+            "Beiersdorf - Kurs: 80,55 €",
+            "BMW St. - Kurs: 104,11 €",
+            "Commerzbank - Kurs: 12,47 €",
+            "Continental - Kurs: 209,94 €",
+            "Daimler - Kurs: 84,33 €"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +42,10 @@ public class EventDiary extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        RecyclerView rv = (RecyclerView) findViewById(R.id.smokeList);
+        rv.setLayoutManager(new LinearLayoutManager(this));
+        MyRecyclerViewAdapter adapter = new MyRecyclerViewAdapter(this, Arrays.asList(smokeList));
+        rv.setAdapter(adapter);
     }
 
     @Override
