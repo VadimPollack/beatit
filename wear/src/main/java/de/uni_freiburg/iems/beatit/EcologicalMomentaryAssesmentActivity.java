e5379c08477aafd132b156177ac4bd7b32a4cd04
Vadim Pollack <v.pollack@web.de>
2018-12-16 21:13:24 +0100

    Der Beschleunigungssensor wird ausgelesen und die Werte werden in das Textfeld reingeschrieben. Im nächsten Schritt kommt das Reinschreiben in eine Datei.

--- wear/src/main/java/de/uni_freiburg/iems/beatit/EcologicalMomentaryAssesmentActivity.java
+++ wear/src/main/java/de/uni_freiburg/iems/beatit/EcologicalMomentaryAssesmentActivity.java
@@ -1,12 +1,20 @@
 package de.uni_freiburg.iems.beatit;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.wearable.activity.WearableActivity;
 import android.widget.TextView;
 
-public class EcologicalMomentaryAssesmentActivity extends WearableActivity {
+public class EcologicalMomentaryAssesmentActivity extends WearableActivity implements SensorEventListener {
 
     private TextView mTextView;
+    private SensorManager mSensorManager;
+    private Sensor mSensorGyroscop;
+    private Sensor mSensorAccelometer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +23,31 @@ public class EcologicalMomentaryAssesmentActivity extends WearableActivity {
 
         mTextView = (TextView) findViewById(R.id.text);
 
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+        mSensorGyroscop = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        mSensorManager.registerListener(this, mSensorGyroscop, SensorManager.SENSOR_DELAY_NORMAL);
+
+        mSensorAccelometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mSensorManager.registerListener(this, mSensorAccelometer, SensorManager.SENSOR_DELAY_NORMAL);
+
         // Enables Always-on
         setAmbientEnabled();
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if(event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            String value = "" +(int)event.values[0] + ";" + (int)event.values[1] + ";" + (int)event.values[2];
+            mTextView.setText(value);
+        }
+        else if (event.sensor.getType() == Sensor.TYPE_GYROSCOPE) {
+
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
