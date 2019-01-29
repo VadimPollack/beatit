132678e9ddb7b8f5b5f329498b350e213a178a5a
Dan Man <daniel.gyarmaty@gmx.de>
2019-01-29 18:37:52 +0100

    Fixed: Wrong return value from Method startSensorMonitoring. Did not affect user experience.

--- wear/src/main/java/de/uni_freiburg/iems/beatit/SensorDataManager.java
+++ wear/src/main/java/de/uni_freiburg/iems/beatit/SensorDataManager.java
@@ -104,7 +104,7 @@ public class SensorDataManager
             }
         }
         isMonitoringStarted.setValue(true);
-        return false;
+        return true;
     }
 
     public boolean stopSensorMonitoring() {
